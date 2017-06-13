c1c480526b1f877e635a9ffe856eed1de7e377bf
Darulos <daryl.bakhuis@gmail.com>
2017-06-13 03:03:33 +0200

    Last fixes

--- GUIDecorator.java
+++ GUIDecorator.java
@@ -3,12 +3,10 @@ package Year_1.Dev02_4;
 
 public abstract class GUIDecorator extends GUIElement implements IDrawable, IUpdateable// TODO: Missing code
 {
-    public Point top_left;
     public GUIElement label;
 
     public GUIDecorator(Point top_left, String text, int size, CustomColor color) {
         super(top_left);
-        this.top_left = top_left;
         this.label = new LabelConstructor().instantiate(text, top_left, size, color);
     }
 
